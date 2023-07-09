77d8f2d2da4482f0da8dbcc1c8a7da9f574718ed
Jason Dev <telegram.org@mail.ru>
2023-07-09 22:38:23 +0500

    first commit

---
+++ src/Log.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Askoldex\Teletant;
+
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+
+class Log extends AbstractLogger
+{
+    /**
+     * @var LoggerInterface|null $logger
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     */
+    public function log($level, string|\Stringable $message, array $context = []): void
+    {
+        if($this->logger instanceof LoggerInterface) {
+            $this->logger->log($level, $message, $context);
+        }
+    }
+
+    /**
+     * @return LoggerInterface|null
+     */
+    public function getLogger(): ?LoggerInterface
+    {
+        return $this->logger;
+    }
+}
